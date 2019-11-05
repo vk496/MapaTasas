82087acf225e1798ed9bd0746c23682eb12df252
Valentín Kivachuk <vk18496@gmail.com>
2019-11-05 22:57:46 +0100

    Fixes for unis.yaml

--- data/uni/extensions.py
+++ data/uni/extensions.py
@@ -14,9 +14,6 @@ def check_tasas_type(value, rule_obj, path):
     if ('cantidades' in value and 'cantidad' in value):
         raise Exception("'%s' and '%s' can't be together in %s" % (data_1, data_2, path))
 
-    if (not 'cantidades' in value and not 'cantidad' in value):
-        raise Exception("Must exist '%s' or '%s' in %s" % (data_1, data_2, path))
-
     if (value['tipo'] == type_1 and data_2 in value):
         raise Exception("For type '%s' must not exist '%s' in %s" % (type_1, data_2, path))
 
